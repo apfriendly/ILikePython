8fc8256ae1ced2ae78dae6cc87a4c12fa21f40f4
FriendlyApp <163114706+apfriendly@users.noreply.github.com>
2024-08-11 13:52:53 +0300

    Update zero.py

--- zero.py
+++ zero.py
@@ -109,3 +109,16 @@ if a3 - a2 == a2 - a1:
     print('YES')
 else:
     print('NO')
+
+
+# Наименьшее из четырёх чисел
+# Напишите программу, которая определяет наименьшее из четырёх чисел.
+
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+if a > b:
+    a = b
+if c > d:
+    c = d
+if a > c:
+    a = c
+print(a)
